c5d212ea50ce84895d5f2ff4093913cbd8256753
Aarthika008 <aarthika@student.tce.edu>
2024-01-08 17:46:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { SlideInterface } from './about/types/slide.interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routing-example';
+  slides: SlideInterface[] = [
+    { url: '/assets/about1.jpg', alt: 'Slide 1' },
+    { url: '/assets/about2.jpg', alt: 'Slide 2' },
+    { url: '/assets/about3.jpg', alt: 'Slide 3' },
+    { url: '/assets/card1.jpg', alt: 'Slide 4' },
+    { url: '/assets/card2.jpg', alt: 'Slide 5' },
+    { url: '/assets/image-1.jpeg', alt: 'beach' },
+    { url: '/assets/image-2.jpeg', alt: 'boat' }
+    
+  ];
+
 }
